ba4c65260408197131e0c2a95f8bd1d6976da433
chenreuven <chen.rvn@gmail.com>
2019-04-09 18:53:50 +0300

    add kalfi component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <kalfi></kalfi>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import Kalfi from "./components/Kalfi.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Kalfi
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Kalfi.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="kalfi">
+    <h1>Kalfi</h1>
+    <div v-for="card in cards">
+      <div>{{card}}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class KalfiComponent extends Vue {
+  cards: string[] = ["A", "B", "C", "D"];
+}
+</script>
